0ddcca85cbdb67532b38d33ab6efa85b8fcb63b2
nathanllee <nathan.l.lee123@gmail.com>
2018-09-02 23:34:48 -0700

    Got roster working

--- roster/views.py
+++ roster/views.py
@@ -3,7 +3,15 @@ from users import models
 
 from django.views.generic import ListView
 # Create your views here.
+from users.models import CustomUser
+
 
 class roster(ListView):
+    model = CustomUser
     template_name = 'roster.html'
-    model = models.CustomUser
+
+    def get_context_data(self, **kwargs):
+        context =super().get_context_data(**kwargs)
+        return context
+    #query_results = CustomUser.objects.all()
+
