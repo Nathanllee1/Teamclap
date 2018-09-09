a857dc61a18882a88736d4ad17a1ec3442421b47
Nathan Lee <36134754+Nathanllee1@users.noreply.github.com>
2018-09-09 10:45:32 -0700

    configured for windows

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
@@ -19,20 +19,29 @@ class BlogDetailView(DetailView):
     template_name = 'blog_detail.html'
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'body', 'picture']
     template_name = 'blog_edit.html'
     success_url = reverse_lazy('home')
+    login_url = 'login'
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'blog_delete.html'
     success_url = reverse_lazy('home')
+    login_url = 'login'
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'article_new.html'
-    fields = ['title', 'body', 'author', 'picture']
+    fields = ['title', 'body', 'picture']
+    success_url = reverse_lazy('home')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
