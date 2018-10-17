e878fcbdea3f98fd03cbfbd0016675a0a02d9352
Nathan Lee <36134754+Nathanllee1@users.noreply.github.com>
2018-10-17 00:20:45 -0700

    Added recipients and tried to fix gunicorn

--- emailSend/forms.py
+++ emailSend/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from users.models import CustomUser
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
@@ -8,4 +8,5 @@ from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 class EmailForm(forms.Form):
     subject = forms.CharField(required=True, label='subject', max_length=100)
     message = forms.CharField(required=True, label='message')
+    recipients = forms.ModelChoiceField(queryset=CustomUser.objects.all())
 
--- emailSend/models.py
+++ emailSend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+class Mail(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    date = models.DateTimeField(auto_now_add=True)
+
+    body = models.TextField()
+
+    picture = models.ImageField(upload_to='pic_folder/', default='pic_folder/None/no-img.jpg')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.id)])
+'''
